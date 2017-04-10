752067006b80e607ab9a9aacfda3fce710b61ad0
Jock Dalby <jock.dalby@education.govt.nz>
2017-04-11 08:45:17 +1200

    add local references to cockpit component

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,28 +8,33 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class CockpitComponent implements OnInit {
 
   // EventEmitter is a generic type where we can define the type of data we plan to emit. The parenthesis at the ned call the EventEmitter constructor function and create a new instance of EventEmitter. The @Output decorator makes the EventEmitter listenable from the outside the component. @Input allows parent component to pass in changes in data. @Output allows parent components to be sent out changes in data. The parenthesis call the @Output method and must be included. You can put an alias inside the parenthesis if you wish to call the EventEmitter something else where the listening takes place i.e. @Output('bpCreated') would be listened to as (bpCreated)='method($event)' in parent component.
+
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = ''; => Needed for 2 way binding
+  // newServerContent = ''; => Needed for 2 way binding
+
+  // Local reference @ ViewChild decorator. Allows you to access an element in the template through local references. See example with serverContentInput in addServer and add Blueprint methods.
+    @ViewChild('serverNameInput') serverNameInput: ElementRef;
+    @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onAddServer() {
+  onAddServer(nameInput, contentInput) {
     this.serverCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput, contentInput) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
